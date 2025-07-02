89367599c972d91161299a8a10f9cb0c9c396bdb
Jon Weider <JonWeider07@gmail.com>
2025-07-02 17:08:56 +0200

    Third commit

--- Bar.js
+++ Bar.js
@@ -8,7 +8,10 @@ function l_bar() {
 function Progress() {
     let x = Number(localStorage.getItem("Hours")) || 0;
     x += 1;
-    if (x > 100) x = 100;
+    if (x > 1000) {
+    document.getElementById("test").innerHTML = "You have completed 1000 hours, you are a pro!";
+    
+    }
     localStorage.setItem("Hours", x);
     document.getElementById("progress-bar").style.width = x/10 + "%";
     document.getElementById("test").innerHTML = "You have completed " + x + " hours";
